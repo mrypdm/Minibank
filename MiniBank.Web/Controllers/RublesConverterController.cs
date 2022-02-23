14a6205dc36c902689619edc23cf96d87b27f6df
mrYPDM <mrypdm@gmail.com>
2022-02-23 15:58:00 +0300

    RublesConverterController added

---
+++ MiniBank.Web/Controllers/RublesConverterController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MiniBank.Core;
+
+namespace MiniBank.Web.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class RublesConverterController : ControllerBase
+    {
+        private readonly IRublesConverter _converter;
+
+        public RublesConverterController(IRublesConverter converter)
+        {
+            _converter = converter;
+        }
+        
+        [HttpGet]
+        public int ConvertRub(int rublesAmount, string currencyCode)
+        {
+            return _converter.Convert(rublesAmount, currencyCode);
+        }
+    }
+}
--- MiniBank.Web/Startup.cs
+++ MiniBank.Web/Startup.cs
@@ -11,6 +11,9 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using MiniBank.Core;
+using MiniBank.Data;
+using MiniBank.Web.Middlewares;
 
 namespace MiniBank.Web
 {
@@ -26,12 +29,14 @@ namespace MiniBank.Web
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "MiniBank.Web", Version = "v1" });
+                c.SwaggerDoc("v1", new OpenApiInfo {Title = "MiniBank.Web", Version = "v1"});
             });
+
+            services.AddScoped<IDatabase, CurrencyRateDatabase>();
+            services.AddScoped<IRublesConverter, RublesConverter>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -43,7 +48,10 @@ namespace MiniBank.Web
                 app.UseSwagger();
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "MiniBank.Web v1"));
             }
-
+            
+            app.UseMiddleware<ExceptionMiddleware>();
+            app.UseMiddleware<UserFriendlyExceptionMiddleware>();
+            
             app.UseHttpsRedirection();
 
             app.UseRouting();
