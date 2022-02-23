3d88379fd08b98d457695aefe27e3a781dc68451
mrYPDM <mrypdm@gmail.com>
2022-02-23 15:51:04 +0300

    Database placeholder added

---
+++ MiniBank.Core/IDatabase.cs
@@ -0,0 +1,10 @@
+﻿using System.Diagnostics.Tracing;
+using System.Linq;
+
+namespace MiniBank.Core
+{
+    public interface IDatabase
+    {
+        int Get(string key);
+    }
+}
---
+++ MiniBank.Data/CurrencyRateDatabase.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using MiniBank.Core;
+
+namespace MiniBank.Data
+{
+    public class CurrencyRateDatabase : IDatabase
+    {
+        private readonly Random _random = new();
+        
+        public int Get(string key)
+        {
+            return _random.Next(0, 100);
+        }
+    }
+}
