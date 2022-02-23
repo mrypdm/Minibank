a599364863cbfbfdb8b0cf54235433ded081b8f7
mrYPDM <mrypdm@gmail.com>
2022-02-23 12:35:28 +0300

    Exceptions added ExceptionMiddlewares added

---
+++ MiniBank.Core/Exceptions/UserFriendlyException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace MiniBank.Core.Exceptions
+{
+    public class UserFriendlyException : Exception
+    {
+        public UserFriendlyException()
+        {
+        }
+
+        public UserFriendlyException(string message) : base(message)
+        {
+        }
+
+        public UserFriendlyException(string message, Exception inner) : base(message, inner)
+        {
+        }
+    }
+}
